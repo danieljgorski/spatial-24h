875ace3a0e50b1b074ac0638b7a06156c10ae1a3
danieljgorski <danieljgorski@gmail.com>
2022-03-31 19:45:29 +0200

    created full analysis script to run downstream scripts

---
+++ scripts/0-full-analysis.R
@@ -0,0 +1,26 @@
+## Libraries, functions and colors----
+library(Seurat) # v4.0.1
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+library(readxl)
+library(yulab.utils)
+library(clusterProfiler)
+library(org.Mm.eg.db)
+library(enrichplot)
+library(ggrepel)
+library(stringr)
+library(xlsx)
+library(RColorBrewer)
+library(scales)
+default_colors <- (hue_pal()(7))
+replicants <- c("#5E4FA2",
+                "#388FBA", 
+                "#A7DBA4",
+                "#E0F298",
+                "#F5FBB0",
+                "#FDD27F",
+                "#F78850",
+                "#D8434D",
+                "#AB1044")
+source("scripts/GOBP_fold_enrich.R")
