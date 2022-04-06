11715460831b4fcfd41b0572b58715e4fbbd734f
danieljgorski <danieljgorski@gmail.com>
2022-04-06 13:00:02 +0200

    Finished full analysis script

--- scripts/0-full-analysis.R
+++ scripts/0-full-analysis.R
@@ -1,28 +1,46 @@
-## Libraries, functions and colors----
-library(Seurat) # v4.0.1
-library(ggplot2)
-library(patchwork)
-library(dplyr)
-library(readxl)
-library(yulab.utils)
-library(clusterProfiler)
-library(org.Mm.eg.db)
-library(enrichplot)
-library(ggrepel)
-library(stringr)
-library(xlsx)
-library(RColorBrewer)
-library(scales)
-default_colors <- (hue_pal()(7))
-replicants <- c(
-  "#5E4FA2",
-  "#388FBA",
-  "#A7DBA4",
-  "#E0F298",
-  "#F5FBB0",
-  "#FDD27F",
-  "#F78850",
-  "#D8434D",
-  "#AB1044"
+# make directories
+output_dirs <- c(
+  "results",
+  "results/basic-figures",
+  "results/cluster-markers",
+  "results/collaborations",
+  "results/collaborations/A01",
+  "results/collaborations/AG-Schrader",
+  "results/collaborations/Alexander-Lang",
+  "results/collaborations/Alexander-Lang/cluster-marker-signatures",
+  "results/collaborations/Alexander-Lang/reference-mapping",
+  "results/collaborations/Alexander-Lang/reference-mapping/day5",
+  "results/differential-abundance",
+  "results/differential-gene-expression",
+  "results/differential-gene-expression/remote-clusters",
+  "results/genes-of-interest",
+  "results/objects",
+  "results/reference-mapping",
+  "results/remote-zone-analysis",
+  "results/signatures-of-interest",
+  "results/spatially-variable-features"
 )
-source("scripts/GOBP_fold_enrich.R")
+
+for (i in output_dirs) {
+  if (!dir.exists(i)) {
+    dir.create(i)
+    print(paste0("made ", i, " directory"))
+  } else {
+    print(paste0(i, " directory already exists."))
+  }
+}
+
+# run analysis in order
+source("scripts/1-clustering.R")
+source("scripts/2-basic-figures.R")
+source("scripts/3-cluster-markers.R")
+source("scripts/4-genes-of-interst.R")
+source("scripts/5-signatures-of-interest.R")
+source("scripts/6-spatially-variable-features.R")
+source("scripts/7-differential-abundance.R")
+source("scripts/8-differential-gene-expression.R")
+source("scripts/9-remote-zone-analysis.R")
+source("scripts/10-reference-mapping.R")
+source("scripts/11-collaboration-A01.R")
+source("scripts/12-collaboration-AG-Schrader.R")
+source("scripts/13-collaboration-Alexander-Lang.R")
